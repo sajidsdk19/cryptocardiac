839740ca13cca1a77ef13023b75251b39c36057b
Sajid Khan <contactdotsajid@gmail.com>
2026-03-04 05:18:40 +0500

    changes again for adsense
    CHANGE

--- src/index.js
+++ src/index.js
@@ -4,23 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
-// react-snap pre-renders pages to static HTML at build time.
-// When a pre-rendered page loads, we hydrate instead of creating a new root
-// so React attaches to the existing HTML without a flash of empty content.
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrateRoot(
-    rootElement,
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-} else {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
